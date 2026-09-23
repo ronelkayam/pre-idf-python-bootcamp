29e2b50a854efa5d564a93856f72f1eaf30f3c5b
ron elkayam <ronelk6@gmail.com>
2026-09-23 19:33:39 +0300

    fString support

--- fString.py
+++ fString.py
@@ -1,5 +1,25 @@
-name = "Ron"
-age = 34
-city = "kiryat-gat"
-newLine = "\n"
-print(f"my name is:{name}{newLine}my age is:{age}{newLine}and i\'m living in:{city}")
+"""
+An f-string (formatted string literal),
+introduced in python 3.6 /
+is a fast , readable, and modern way to embed variables and
+expressions directly inside string
+always the type of variable stay as declaration
+"""
+# the ols way
+# print("my age is :" + 36)  # failed
+print("my age is :" + str(36))
+
+name = "Alex"
+age = 30
+price = 45.6783423452352
+# basic variable insertion
+print(f"Hello my name is {name} and I am {age} years old")
+
+# Inline Math/ expression
+print(f"In 5 years , you will be {age + 5}.")
+
+#String methods
+print(f"Name in uppercase is {name.upper()}")
+
+#number formating (2 decimal places)
+print(f"Total salary is ${price:.2f}")
